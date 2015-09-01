6372b71883525f533d6dd42660d80187c59d2a93
Madhusudhan Rao <morusu.madhu@ymail.com>
2015-09-01 23:41:26 +0530

    Initial Commit

---
+++ Roamy/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Roamy
+//
+//  Created by Madhu Morusu on 01/09/15.
+//  Copyright (c) 2015 madhu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Roamy/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Roamy
+//
+//  Created by Madhu Morusu on 01/09/15.
+//  Copyright (c) 2015 madhu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
